f0904e894122e11ff3ff0bfc48963cdfc8ddb961
Erick-EEV <EEVERICKEEV@gmail.com>
2021-07-12 12:29:32 -0400

    ES5 Function

---
+++ src/components/ArrowFunctionExample.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class ArrowFunctionExample extends Component {
+    render() {
+
+
+
+
+        //ES5 Function
+        function helloWorld() {
+            return console.log("Hello World")
+        }
+        
+
+
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ArrowFunctionExample;
